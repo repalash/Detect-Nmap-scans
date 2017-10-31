633e70c39241d1507a4aeeb07f2c6b41b9f34cdc
Sambhav Satija <Sambhav13085@iiitd.ac.in>
2017-10-31 22:19:56 +0530

    Fixed linting error of nfmodule.c

--- nfmodule.c
+++ nfmodule.c
@@ -10,22 +10,22 @@ static struct nf_hook_ops nfho;         //struct holding set of hook function op
 
 struct tcphdr
 {
-	u_int16_t source;
-	u_int16_t dest;
-	u_int32_t seq;
-	u_int32_t ack_seq;
+    u_int16_t source;
+    u_int16_t dest;
+    u_int32_t seq;
+    u_int32_t ack_seq;
 #  if __BYTE_ORDER == __LITTLE_ENDIAN
-	u_int16_t res1:4;
-	u_int16_t doff:4;
-	u_int16_t fin:1;
-	u_int16_t syn:1;
-	u_int16_t rst:1;
-	u_int16_t psh:1;
-	u_int16_t ack:1;
-	u_int16_t urg:1;
-	u_int16_t res2:2;
+    u_int16_t res1:4;
+    u_int16_t doff:4;
+    u_int16_t fin:1;
+    u_int16_t syn:1;
+    u_int16_t rst:1;
+    u_int16_t psh:1;
+    u_int16_t ack:1;
+    u_int16_t urg:1;
+    u_int16_t res2:2;
 #  elif __BYTE_ORDER == __BIG_ENDIAN
-	u_int16_t doff:4;
+    u_int16_t doff:4;
     u_int16_t res1:4;
     u_int16_t res2:2;
     u_int16_t urg:1;
@@ -37,9 +37,9 @@ struct tcphdr
 #  else
 #   error "Adjust your <bits/endian.h> defines"
 #  endif
-	u_int16_t window;
-	u_int16_t check;
-	u_int16_t urg_ptr;
+    u_int16_t window;
+    u_int16_t check;
+    u_int16_t urg_ptr;
 };
 
 
